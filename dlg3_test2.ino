41fe244fc5b049ee0081f7f382f3d9e89ad27d39
Mark P Sullivan <mark@RockTheBike.com>
2015-02-22 21:06:13 -0800

    Prepare to move features from test to real code

---
+++ dlg3_test2.ino
@@ -0,0 +1,266 @@
+// this file tracks the features we have not yet ported from dlg3_test2.ino to the nice state machine
+
+// but we don't want to compile it
+#if 0
+
+#define BUTTON_SENSE 2  // PD2/2
+#define ONFET        4  // PD4/4
+#define CCFL_PIN     5  // pin 10 old, pin 5 new (for 32khz special)
+#define LED_PIN      7  // PD7/7
+#define CHARGE123    9  // PB1/9 prototype
+#define DRAIN1       11 // PB3/11 prototype
+#define DRAIN2       3  // PD3/3
+#define DRAIN3       10 // PB2/10 prototype
+#define BOOST        6  // PD6/6 prototype
+#define B1THERM      A0
+#define B2THERM      A1
+#define B3THERM      A2
+#define B1P          A3
+#define B2P          A4
+#define BATTERY      A5
+#define CCFL_SENSE   A6
+#define JACK_SENSE   A7
+
+#define B1P_COEFF     203.518 // ADC counts per volt
+#define B2P_COEFF     122.460
+#define BATTERY_COEFF 81.936
+#define BATT_EMPTY   2.95 // how many volts to DIE at
+
+// how much current we should have at brightest point of pattern
+int ideal_max_current = 450; // 175 with built-in vref
+#define MAX_PWM 250
+int pwm_for_max_brightness = 80;
+int jumpVal = 1;
+int offCount = 0;  // counts how many off requests we've seen
+#define OFF_THRESH 5 // how many to make us turn off
+unsigned long senseRead = 0;
+#define DELAYFACTOR 64 // millis() and delay() this many times faster  [fake/real]
+float batt1,batt2,batt3; // voltage of battery cells
+int debugMode = 0; // allows for debugging modes to be triggered in production software
+unsigned long tickcount = 0;
+
+void setup() {
+pinMode(ONFET,OUTPUT);
+digitalWrite(ONFET,HIGH);  // keep power on
+
+pinMode(LED_PIN,OUTPUT);
+digitalWrite(LED_PIN,HIGH);  // LED ON
+
+pinMode(CCFL_PIN,OUTPUT); // 32khz
+// setPwmFrequency(CCFL_PIN,1); // set PWM freq to 31,250 Hz
+// WGM02 = 0, WGM01 = 1, WGM00 = 1 see page 108
+// COM0B1 = 1, COM0B0 = 0 see page 107
+// CS02 = 0, CS01 = 0, CS00 = 1 see page 110
+  CLKPR = 0x80;  // enable write to clkps see page 37
+  CLKPR = 0x00;  // set divisor to 1  see page 38
+  TCCR0A = 0b10100011;
+  TCCR0B = 0b00000001;
+  Serial.begin(76800); // to get 38400 baud, put 76800 baud here
+}
+
+void loop() {
+#ifdef REPRORT_TIMING
+  Serial.print("Starting loop ");
+  Serial.print(tickcount);
+  Serial.print(" at ");
+  Serial.println(millis()/DELAYFACTOR);
+#endif
+  // TODO:  do we want a full PID system here?
+
+  // first we calibrate pwm_for_max_brightness to ideal_max_current and the hardware's instantaneous response
+  senseRead = averageRead(CCFL_SENSE);
+  // what we should see with pwm_for_max_brightness perfectly calibrated to ideal_max_current
+  float ideal_instantaneous_current = ideal_max_current * SOME_FACTOR * pattern_brightness*pattern_brightness;
+
+  int t = millis()/DELAYFACTOR % 1000;
+  pattern_brightness =
+    t < 500 ? t/500 : (1000-t)/500;
+  if (senseRead < ideal_instantaneous_current) {
+    digitalWrite(LED_PIN,HIGH);  // LED ON
+    actual pwm_for_max_brightness += jumpVal;
+    if (pwm_for_max_brightness > MAX_PWM) pwm_for_max_brightness = MAX_PWM;
+  }
+  if (senseRead > ideal_instantaneous_current) {
+    digitalWrite(LED_PIN,LOW);  // LED OFF
+    pwm_for_max_brightness -= jumpVal;
+    if (pwm_for_max_brightness < 1) pwm_for_max_brightness = 1;
+  }
+  analogWrite(CCFL_PIN, pwm_for_max_brightness*pattern_brightness);
+
+  getBattVoltages();
+  printAnalogs();
+
+  if (batt1 < BATT_EMPTY) die("batt1 dead! ");
+  if (batt2 < BATT_EMPTY) die("batt2 dead! ");
+  if (batt3 < BATT_EMPTY) die("batt3 dead! ");
+
+  delay(25*DELAYFACTOR); // actual time = n * DELAYFACTOR milliseconds
+  
+  if (digitalRead(BUTTON_SENSE)) {
+    Serial.print("+");
+    offCount++;
+  } else {
+    offCount = 0; // if (--offCount < 0) offCount = 0;
+  }
+
+  if (offCount > OFF_THRESH) die("offCount > OFF_THRESH");
+#ifdef REPRORT_TIMING
+  Serial.print("  Ending loop ");
+  Serial.print(tickcount);
+  Serial.print(" at ");
+  Serial.println(millis()/DELAYFACTOR);
+#endif
+  tickcount++;
+}
+
+float averageRead(int pin) {
+  #define READ_MULTITUDE 50
+  float analogAdder = 0;
+  for (int i = 0; i < READ_MULTITUDE; i++) analogAdder += analogRead(pin);
+  analogAdder /= READ_MULTITUDE;
+  return analogAdder;
+}
+
+void getBattVoltages() {
+  batt1 = averageRead(B1P) / B1P_COEFF;
+  batt2 = (averageRead(B2P) / B2P_COEFF) - batt1;
+  batt3 = (averageRead(BATTERY) / BATTERY_COEFF) - batt2 - batt1;
+}
+
+void die(const char* reason) {
+  Serial.print("off because: ");
+  Serial.println(reason);
+  digitalWrite(LED_PIN,HIGH);  // LED ON
+  digitalWrite(ONFET,LOW); // turn power off
+  analogWrite(CCFL_PIN,0); // turn off ccfl
+  unsigned int growing_delay = 10000;
+  while (true) { // wait here until they let go of button
+    Serial.print(reason);
+    getBattVoltages();
+    printAnalogs();
+    delay(growing_delay*DELAYFACTOR);
+    growing_delay += 10000;
+  }
+}
+
+void printAnalogs() {
+  Serial.print(millis()/DELAYFACTOR);
+  Serial.print(":");
+  Serial.print(tickcount);
+  Serial.print("  ");
+  if (debugMode > 0) { // print detailed battery analogRead information
+    Serial.print(averageRead(B1P),3);
+    Serial.print(" B1P  (");
+    Serial.print(averageRead(B1P)/B1P_COEFF,3);
+    Serial.print(")   ");
+    Serial.print(averageRead(B2P),3);
+    Serial.print(" B2P  (");
+    Serial.print(averageRead(B2P)/B2P_COEFF,3);
+    Serial.print(")   ");
+
+    Serial.print(averageRead(BATTERY),3);
+    Serial.print(" BATTERY  (");
+    Serial.print(averageRead(BATTERY)/BATTERY_COEFF,3);
+    Serial.print(")      TH1: ");
+    Serial.print(averageRead(B1THERM),2);
+    Serial.print("  TH2: ");
+    Serial.print(averageRead(B2THERM),2);
+    Serial.print("  TH3: ");
+    Serial.print(averageRead(B3THERM),2);
+  } else {
+    Serial.print("batt1=");
+    Serial.print(batt1,3);
+    Serial.print("   batt2=");
+    Serial.print(batt2,3);
+    Serial.print("   batt3=");
+    Serial.print(batt3,3);
+    Serial.print("   pwm_for_max_brightness=");
+    Serial.print(pwm_for_max_brightness);
+    Serial.print("   senseRead=");
+    Serial.print(senseRead);
+  }
+  Serial.println("");
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+    case 1: 
+      mode = 0x01; 
+      break;
+    case 8: 
+      mode = 0x02; 
+      break;
+    case 64: 
+      mode = 0x03; 
+      break;
+    case 256: 
+      mode = 0x04; 
+      break;
+    case 1024: 
+      mode = 0x05; 
+      break;
+    default: 
+      return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } 
+    else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } 
+  else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+    case 1: 
+      mode = 0x01; 
+      break;
+    case 8: 
+      mode = 0x02; 
+      break;
+    case 32: 
+      mode = 0x03; 
+      break;
+    case 64: 
+      mode = 0x04; 
+      break;
+    case 128: 
+      mode = 0x05; 
+      break;
+    case 256: 
+      mode = 0x06; 
+      break;
+    case 1024: 
+      mode = 0x7; 
+      break;
+    default: 
+      return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+/*     voltage: 4.0v        8.0v             12.0v
+ *     206 per volt         123 per volt     82 per volt
+ *     339 B1P  (33A)   3DD B2P  (3E0)   3E3 BATTERY  (3E3)      TH1: 304  TH2: 312  TH3: 30C
+ *     339 B1P  (33A)   3DC B2P  (3DF)   3E0 BATTERY  (3E2)      TH1: 329  TH2: 319  TH3: 309
+ *     339 B1P  (339)   3DD B2P  (3DF)   3E3 BATTERY  (3E0)      TH1: 321  TH2: 312  TH3: 30D
+ *     339 B1P  (33A)   3DA B2P  (3E0)   3E0 BATTERY  (3E2)      TH1: 308  TH2: 30E  TH3: 30D
+ *     339 B1P  (338)   3DA B2P  (3E0)   3E0 BATTERY  (3E3)      TH1: 329  TH2: 317  TH3: 30C
+ *     33A B1P  (339)   3DC B2P  (3E1)   3E0 BATTERY  (3E1)      TH1: 326  TH2: 311  TH3: 30C
+ *     33A B1P  (339)   3DA B2P  (3DF)   3E2 BATTERY  (3E3)      TH1: 300  TH2: 30F  TH3: 30D
+ *     339 B1P  (339)   3DD B2P  (3C8)   3E1 BATTERY  (3E5)      TH1: 305  TH2: 310  TH3: 30C
+ *     339 B1P  (339)   3DC B2P  (3DE)   3E2 BATTERY  (3E1)      TH1: 30A  TH2: 312  TH3: 30C
+ *     339 B1P  (339)   3DD B2P  (3DF)   3E1 BATTERY  (3E4)      TH1: 31D  TH2: 30F  TH3: 30F
+ *     339 B1P  (335)   3DA B2P  (3E1)   3E1 BATTERY  (3E1)      TH1: 326  TH2: 318  TH3: 30C
+ *     339 B1P  (338)   3DA B2P  (3DF)   3E1 BATTERY  (3E5)      TH1: 320  TH2: 315  TH3: 308
+ *     33A B1P  (338)   3DB B2P  (3E4)   3E2 BATTERY  (3E1)      TH1: 31A  TH2: 30D  TH3: 30D
+ *     339 B1P  (33A)   3DA B2P  (3DF)   3E1 BATTERY  (3E3)      TH1: 308  TH2: 311  TH3: 30D
+ *     339 B1P  (339)   3DA B2P  (3DF)   3E1 BATTERY  (3E4)      TH1: 325  TH2: 316  TH3: 309
+ *     339 B1P  (339)   3DD B2P  (3DF)   3E3 BATTERY  (3E0)      TH1: 326  TH2: 318  TH3: 30A
+ *     339 B1P  (339)   3DC B2P  (3DF)   3E2 BATTERY  (3E0)      TH1: 324  TH2: 317  TH3: 30C
+ *     339 B1P  (339)   3DD B2P  (3DF)   3E3 BATTERY  (3E1)      TH1: 326  TH2: 315  TH3: 30D
+ */
+
+#endif
